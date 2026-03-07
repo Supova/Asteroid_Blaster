9498b3e163999897565b1f7eb5dc62c0fb7a0bb1
Supova <no-reply@domain.com>
2026-03-06 17:13:22 -0800

    (chore) project setup

---
+++ assets.h
@@ -0,0 +1,42 @@
+#ifndef ASSETS_H
+#define ASSETS_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+//--------------------STRUCTS--------------------------------------------------------------------------------------
+struct position {
+    uint8_t x;
+    uint8_t y;
+};
+
+struct bullet_t {
+    // char bullet = "|"
+    uint8_t id;
+    uint8_t x;
+    uint8_t y;
+    bool in_frame;
+};
+
+struct asteroid_t {
+    uint8_t id;
+    uint8_t x;
+    uint8_t y;
+    bool in_frame; // used as flag for active asteroids
+};
+
+struct board {
+    // struct ship player;
+    // array of structs
+    struct bullet_t bullets[10];
+    struct asteroid_t asteroids[10];
+    // score
+};
+
+struct ship {
+    uint8_t x;
+    uint8_t y;
+    // health
+};
+
+#endif
