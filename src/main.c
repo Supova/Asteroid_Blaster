1e672b722a0b9ce2018d850b7889951814d62c35
Supova <no-reply@domain.com>
2026-03-09 15:31:02 -0700

    (feat) add game start logic

--- src/main.c
+++ src/main.c
@@ -3,21 +3,23 @@
 #include "render.h"
 #include "uart.h"
 #include "utils.h"
+#include "game.h"
 
 int main() {
-    uart_init();
+    uart_init();  
 
     output_string(prompt_game_welcome);
     output_string(prompt_game_rules);
     output_string(prompt_game_beginning);
 
-    // wait for user to start game
     char response = ' ';
     while (response != 'y') {
         response = uart_read_blocking();
     }
 
     uart_interrupt_init();
+    start_game();
+
     while (1) {
     }
 }
